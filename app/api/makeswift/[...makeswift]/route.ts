dc80375a9f8bb0e8cdfa4601ba6878a875eff8c3
marcoenjoy <91163720+marcoenjoy@users.noreply.github.com>
2025-11-11 22:23:16 +0100

    Create route.ts

---
+++ app/api/makeswift/[...makeswift]/route.ts
@@ -0,0 +1,10 @@
+import { MakeswiftApiHandler } from '@makeswift/runtime/next/server';
+import { strict } from 'assert';
+import { runtime } from '../../../makeswift/runtime';
+import '../../../makeswift/components';
+
+strict(process.env.MAKESWIFT_SITE_API_KEY, 'MAKESWIFT_SITE_API_KEY is required');
+
+const handler = MakeswiftApiHandler(process.env.MAKESWIFT_SITE_API_KEY!, { runtime });
+
+export { handler as GET, handler as POST, handler as OPTIONS };
