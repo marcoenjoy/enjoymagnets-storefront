108214809422f67fad90137eb97178769c0ac9a1
Marco EnjoyMagnets <git@enjoymagnets.com>
2025-12-18 20:25:11 +0100

    fix: keep only App Router makeswift catch-all

--- app/api/makeswift/[[...makeswift]]/route.ts
+++ app/api/makeswift/[[...makeswift]]/route.ts
@@ -1,6 +1,9 @@
 import { MakeswiftApiHandler } from '@makeswift/runtime/next/server'
+
 import { runtime } from '../../../makeswift/runtime'
 import '../../../makeswift/components'
+
+export const dynamic = 'force-dynamic'
 
 const handler = MakeswiftApiHandler(
   process.env.MAKESWIFT_SITE_API_KEY as string,
